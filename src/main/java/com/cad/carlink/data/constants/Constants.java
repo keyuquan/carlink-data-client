d9f14db820ace9ddf887fc68ac936f3ddafd4ae0
keyuquan <9298242382@qq.com>
2018-12-20 11:16:58 +0800

    数据位置修改

--- src/main/java/com/cad/carlink/data/constants/Constants.java
+++ src/main/java/com/cad/carlink/data/constants/Constants.java
@@ -6,15 +6,15 @@ public class Constants {
     public static String apiCatId = "21";
 
     public static String TABLE_KL_TF_PT_CMPSN = "KL_TF_PT_CMPSN";
-    public static String LOG_PATH_KL_TF_PT_CMPSN = "./logs/kl_tf_pt_cmpsn_";
+    public static String LOG_PATH_KL_TF_PT_CMPSN = "./data/kl_tf_pt_cmpsn_";
 
     public static String TABLE_KL_NEW_TF_BS_JURCTC_WXJY = "KL_NEW_TF_BS_JURCTC_WXJY";
-    public static String LOG_KL_NEW_TF_BS_JURCTC_WXJY = "./logs/kl_new_tf_bs_jurctc_wxjy_";
+    public static String LOG_KL_NEW_TF_BS_JURCTC_WXJY = "./data/kl_new_tf_bs_jurctc_wxjy_";
 
     public static String TABLE_IMP_TI_VECGOODS = "IMP_TI_VECGOODS";
-    public static String LOG_KL_IMP_TI_VECGOODS = "./logs/imp_ti_vecgoods_";
+    public static String LOG_KL_IMP_TI_VECGOODS = "./data/imp_ti_vecgoods_";
 
     public static String TABLE_IMP_TI_OWNER = "IMP_TI_OWNER";
-    public static String LOG_IMP_TI_OWNER = "./logs/imp_ti_owner_";
+    public static String LOG_IMP_TI_OWNER = "./data/imp_ti_owner_";
 
 }
